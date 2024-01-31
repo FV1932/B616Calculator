9adf7ded56f3a1238142d68d36a674f7adb49fe6
JacobLiu <jacobliu001@qq.com>
2024-01-31 12:43:33 +0000

    Major refractor: use requests-cache instead of caching manually.

--- request_handler.py
+++ request_handler.py
@@ -1,9 +1,9 @@
-# Yes this is a (very) budget implementation of something that resembles requests-cache
-import requests
+import requests_cache
 from urllib.parse import urljoin
 import pathlib
 from base64 import b16decode, b16encode
 import logging
+import time
 
 logger = logging.getLogger(__name__)
 
@@ -11,32 +11,22 @@ BASE_URL = "https://arcwiki.mcd.blue/"
 SITE_CACHE = pathlib.Path("./site_cache/")
 
 
-def encode_directory(url_path: str) -> str:
-    return b16encode(url_path.encode()).decode()
+session = requests_cache.CachedSession("arcwiki_cache")
+
+def make_throttle_hook(timeout: float = 1.0):
+    def hook(response, *args, **kwargs):
+        if not getattr(response, "from_cache", False):
+            logger.info(f"Cache miss at {response.url}")
+            time.sleep(timeout)
+        return response
+    return hook
+
+session.hooks['response'].append(make_throttle_hook(1))
 
 
-def decode_directory(local_path: str) -> str:
-    return b16decode(local_path).decode()
-
-def hash_encoded_dir(encoded_dir: str) -> str:
-    return f"{(hash(encoded_dir)&0xFF):02x}"
-
-
-def get_resource(path: str, force_flush_cache: bool = False) -> str:
-    encoded_dir = encode_directory(path)
-    local_cache_path = pathlib.Path(SITE_CACHE, hash_encoded_dir(encoded_dir), encoded_dir)
+def get_resource(path: str, refresh: bool = False) -> str:
     full_url = urljoin(BASE_URL, path)
-
-    if local_cache_path.is_file() and not force_flush_cache:
-        logger.info(f"Cache hit for resource at {path}")
-        with open(local_cache_path, "r") as f:
-            return "\n".join(f.readlines())
-
-    logger.info(f"Cache miss for resource at {path}")
-    res = requests.get(full_url)
+    res = session.get(full_url, refresh)
     if res.status_code >= 300:
         raise RuntimeError(f"Failed to get resource at {full_url}")
-    local_cache_path.parent.mkdir(parents=True, exist_ok=True)
-    with open(local_cache_path, "w") as f:
-        f.write(res.text)
     return res.text
