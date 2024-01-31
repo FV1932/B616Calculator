88a90d4049a3a5fd0d15c14d4d62ebb8bc85bf70
JacobLiu <jacobliu001@qq.com>
2024-01-31 10:27:47 +0000

    Basic caching via a resource request handler.

--- get_song_img_link.py
+++ get_song_img_link.py
@@ -3,28 +3,25 @@ from cache_handler import CacheHandler
 import lxml.html as html
 from urllib.parse import urljoin
 import logging
+from request_handler import get_resource
 
-BASE_URL = "https://arcwiki.mcd.blue/"
-
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 def make_cover_url(song_id: str, *, force_flush_cache = False):
     with CacheHandler(song_id) as ch:
         if not force_flush_cache:
             url = ch.get_cover_url()
             if url:
-                logging.info(f"Returning cover url of {song_id}: {url}")
+                logger.info(f"Returning cover url of {song_id}: {url}")
                 return url
         imagePageUrl = f"/File:Songs_{song_id}.jpg"
-        res = requests.get(urljoin(BASE_URL, url=imagePageUrl))
-        if res.status_code >= 300:
-            raise RuntimeError(
-                f"Request to obtain song cover URL for {song_id} failed with {res.status_code}."
-            )
-        doc_root = html.document_fromstring(res.text)
+        res = get_resource(imagePageUrl)
+        doc_root = html.document_fromstring(res)
         link_div = doc_root.find_class("internal")[0]
         cover_url = link_div.get("href")
         ch.set_cover_url(cover_url)
-        logging.info(f"Set cover url of {song_id} to {cover_url}")
+        logger.info(f"Set cover url of {song_id} to {cover_url}")
 
 
 if __name__ == "__main__":
--- request_handler.py
+++ request_handler.py
@@ -1,15 +1,38 @@
 import requests
 from urllib.parse import urljoin
+import pathlib
 from base64 import b16decode, b16encode
+import logging
+
+logger = logging.getLogger(__name__)
 
 BASE_URL = "https://arcwiki.mcd.blue/"
-SITE_CACHE = "./site_cache/"
+SITE_CACHE = pathlib.Path("./site_cache/")
+
 
 def encode_directory(url_path: str) -> str:
     return b16encode(url_path.encode()).decode()
+
 
 def decode_directory(local_path: str) -> str:
     return b16decode(local_path).decode()
 
 
+def get_resource(path: str, force_flush_cache: bool = False) -> str:
+    encoded_dir = encode_directory(path)
+    local_cache_path = pathlib.Path(SITE_CACHE, encoded_dir[:2], encoded_dir)
+    full_url = urljoin(BASE_URL, path)
+    
+    if local_cache_path.is_file() and not force_flush_cache:
+        logger.info(f"Cache hit for resource at {path}")
+        with open(local_cache_path, "r") as f:
+            return "\n".join(f.readlines())
 
+    logger.info(f"Cache miss for resource at {path}")
+    res = requests.get(full_url)
+    if res.status_code >= 300:
+        raise RuntimeError(f"Failed to get resource at {full_url}")
+    local_cache_path.parent.mkdir(parents=True, exist_ok=True)
+    with open(local_cache_path, "w") as f:
+        f.write(res.text)
+    return res.text
