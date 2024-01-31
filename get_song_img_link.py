5a67b8ebcc094f1dbc24ecabd4e27f29d7827d46
JacobLiu <jacobliu001@qq.com>
2024-01-31 06:37:38 +0000

    Refractor: put cache handler in separate file.

--- get_song_img_link.py
+++ get_song_img_link.py
@@ -1,48 +1,5 @@
-from typing import Any
 import requests
-import lxml.html as html
-import os
-import pathlib
-import json
-
-
-class CacheHandler:
-    """Handles local caches for a song
-
-    N.B. NOT THREAD SAFE
-    """
-
-    LOCAL_CACHE = "./cache/"
-
-    @staticmethod
-    def getMainCachePath(song_id) -> pathlib.Path:
-        return pathlib.Path(CacheHandler.LOCAL_CACHE, song_id, "main.json")
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, type, value, traceback):
-        self.mainPath.parent.mkdir(parents=True, exist_ok=True)
-        with open(self.mainPath, "w") as f:
-            json.dump(self.__obj, f)
-
-    def __init__(self, song_id: str) -> None:
-        self.song_id: str = str(song_id)
-        self.mainPath = CacheHandler.getMainCachePath(self.song_id)
-        if self.mainPath.is_file():
-            with open(self.mainPath, "r") as f:
-                self.__obj = json.load(f)
-        else:
-            self.__obj = {}
-
-    def __getitem__(self, name: str) -> Any:
-        return self.__obj[name]
-
-    def __setitem__(self, name: str, newval):
-        self.__obj[name] = newval
-
-    def get_cover_url(self) -> str | None:
-        return self["cover_url"]
+from cache_handler import CacheHandler
 
 
 def make_cover_url(song_id: str):
